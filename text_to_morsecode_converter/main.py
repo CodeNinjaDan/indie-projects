dca119521b185ff46c8915d0b331d4530159423f
CodeNinjaDan <dnlmuiru@gmail.com>
2025-06-01 23:07:11 +0300

    added error handling

--- text_to_morsecode_converter/main.py
+++ text_to_morsecode_converter/main.py
@@ -22,10 +22,29 @@ MORSE_CODE_DICT = {
 
 def translate():
     user_input = input('What would you like to translate to Morse Code?\n').upper()
-    morse_translation = ' '.join(MORSE_CODE_DICT[letter] for letter in user_input)
-    print(morse_translation)
 
-    translate_more()
+        # Error handling to show unconvertible characters
+
+        # invalid_chars = [char for char in user_input if char not in MORSE_CODE_DICT]
+        #
+        # if invalid_chars:
+        #     print(f"Sorry, the following character(s) cannot be converted to Morse Code: {', '.join(invalid_chars)}")
+        #     print("Try a different input.")
+        #     translate()
+        # else:
+        #     morse_translation = ' '.join(MORSE_CODE_DICT[letter] for letter in user_input)
+        #     print(morse_translation)
+        #     translate_more()
+    try:
+        morse_translation = ' '.join(MORSE_CODE_DICT[letter] for letter in user_input)
+        print(morse_translation)
+
+        translate_more()
+
+    except KeyError:
+        print('Character(s) entered cannot be translated to Morse Code :( \nTry a different character(s)')
+        translate()
+
 
 def translate_more():
     more_words = input('Would you like to translate more words (Y/n)? \n').upper()

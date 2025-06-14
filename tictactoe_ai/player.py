81b0b37ff76d7ae6c61e1414c4a58c18cbb44463
CodeNinjaDan <dnlmuiru@gmail.com>
2025-06-14 23:28:42 +0300

    add minimax function

--- tictactoe_ai/player.py
+++ tictactoe_ai/player.py
@@ -1,4 +1,5 @@
 import random
+import math
 
 class Player:
     def __init__(self, letter):
@@ -37,3 +38,56 @@ class HumanPlayer(Player):
                 print('Invalid square. Please try again.')
 
         return val
+
+
+class ProComputerPlayer(Player):
+    def __init__(self, letter):
+        super().__init__(letter)
+
+    def get_move(self, game):
+        if len(game.available_moves()) == 9:
+            square = random.choice(game.available_moves())
+        else:
+            # get square based off minimax algorithm
+            square = self.minimax(game, self.letter)['position']
+        return square
+
+    def minimax(self, snap, player):  # snap represents the game taking a snapshot of the board at every stage
+        max_player = self.letter
+        other_player = 'O' if player == 'X' else 'X'
+
+        # check if previous move is a winner
+        # base case
+        if snap.current_winner == other_player:
+            # return position and score 4 minimax to work
+            return {'position': None,
+                    'score': 1 * (snap.num_empty_squares() + 1) if other_player == max_player else -1 * (
+                                snap.num_empty_squares() + 1)}
+
+        elif not snap.empty_squares():
+            return {'position': None, 'score': 0}
+
+        # Initialize some dictionaries
+        if player == max_player:
+            best = {'position': None, 'score': -math.inf}  # maximize each score
+        else:
+            best = {'position': None, 'score': math.inf}  # minimize each score
+
+        for possible_move in snap.available_moves():
+            # 1. make a move try that spot
+            snap.make_move(possible_move, player)
+            # 2. recurse using minimax to simulate a game after that move
+            sim_score = self.minimax(snap, other_player)  # alternate players
+            # 3. undo the move
+            snap.board[possible_move] = ' '
+            snap.current_winner = None
+            sim_score['position'] = possible_move
+            # 4. update the dictionaries if necessary
+            if player == max_player:
+                if sim_score['score'] > best['score']:
+                    best = sim_score
+            else:
+                if sim_score['score'] < best['score']:
+                    best = sim_score
+
+        return best
